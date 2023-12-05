90b763c4480c8f63c526bf0dc97036e4ddee21e8
Adrian Todd <solarshado@users.noreply.github.com>
2023-12-04 19:18:20 -0500

    day 3, part 1, done

---
+++ 2023/d3/part1.ts
@@ -0,0 +1,157 @@
+import { linesFrom, sum } from "../util.ts";
+
+const example = `
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+`.trim();
+
+function isDigit(s:string):boolean {
+    return !isNaN(parseInt(s));
+}
+
+function isSymbol(s:string):boolean {
+    return s != '.' && !isDigit(s);
+}
+
+type GridNumber = {
+  value: number;
+  originX: number;
+  originY: number;
+};
+
+class Grid {
+    private readonly data:string[];
+    public readonly width:number;
+    public readonly height:number;
+
+    constructor(source:string|string[]) {
+        const data = Array.isArray(source) ? [...source] : source.split('\n');
+        this.data = data;
+        this.height = data.length;
+        this.width = data[0].length
+    }
+
+    get(x:number, y:number):string {
+        return this.data[y][x];
+    }
+
+    getNeighborhood(x:number, y:number):string[] {
+        const minX = Math.max(x-1,0);
+        const maxX = Math.min(x+1,this.width-1);
+        const minY = Math.max(y-1,0);
+        const maxY = Math.min(y+1,this.height-1);
+
+        //console.log(`getNeighborhood(${x},${y}):`,{minX,maxX,minY,maxY});
+
+        const ret = [];
+
+        for(let y = minY; y < maxY+1; ++y) {
+            ret.push(this.data[y].substring(minX,maxX+1));
+        }
+
+        return ret;
+    }
+
+    /** @deprecated broken, do not use */
+    getFullNumberAt(x:number, y:number):GridNumber|null {
+        const row = this.data[y];
+        const startPoint = row[x];
+        if(!isDigit(startPoint))
+            return null;
+
+        let strNum = startPoint;
+        let originX = x;
+
+        for(let _x = x+1; _x < this.width; ++_x) {
+            const c = row[_x];
+            if(!isDigit(c))
+                break;
+            strNum = strNum + c;
+        }
+
+        for(let _x = x-1; _x < 0; ++_x) {
+            const c = row[_x];
+            if(!isDigit(c))
+                break;
+            strNum = c + strNum;
+            originX = _x;
+        }
+
+        return {value: +strNum, originX, originY: y};
+    }
+}
+
+async function main() {
+    const schematic = await linesFrom();
+    //const schematic = example;
+
+    const grid = new Grid(schematic);
+
+    console.log(grid);
+
+    /*
+    let v = "";
+    do {
+        v = prompt("num at? (yeet to bail)") ?? "";
+        const [x=0,y=0] = v.split(',').map(n=>+n);
+        const at = grid.get(x,y);
+        const ns = grid.getFullNumberAt(x,y);
+        console.log(x,y,at);
+        console.log(ns);
+    } while(v!='yeet');
+    */
+
+    const goodNums = [];
+
+    // not optimally inefficient but whatever
+    for(let y = 0; y < grid.height; ++y)
+        for(let x = 0; x < grid.width; ++x) {
+            let curr = grid.get(x,y);
+            console.log(`on: ${x},${y}: ${curr}`);
+            if(!isDigit(curr))
+                continue;
+
+            //console.log('found digit');
+
+            let foundSymbol = false
+            let acc = "";
+
+            do {
+                acc += curr;
+
+                console.log(`inner loop: ${x},${y}; foundSym? ${foundSymbol} ; acc = '${acc}'`);
+
+                const neighbors = grid.getNeighborhood(x,y);
+                console.log(`neighbors: ${neighbors}`);
+                if(neighbors.some(s=>[...s].some(isSymbol)))
+                    foundSymbol = true;
+
+                ++x;
+                if(x > grid.width)
+                    break;
+                curr = grid.get(x,y);
+            } while(isDigit(curr));
+
+            console.log(`left inner loop: ${x},${y}; foundSym? ${foundSymbol} ; acc = '${acc}'`);
+
+            if(foundSymbol)
+                goodNums.push(+acc);
+
+            console.log(`end of outer loop; goodNums: [${goodNums}]'`);
+        }
+
+    console.log(goodNums);
+
+    console.log(sum(goodNums));
+}
+
+if(import.meta.main)
+    await main();
--- 2023/util.ts
+++ 2023/util.ts
@@ -11,3 +11,11 @@ export async function linesFrom(source:Deno.Reader = Deno.stdin):Promise<string[
     return ret;
 }
 
+export function sum<T=number>(nums:T[], mapper?:((v:T)=>number)):number {
+    const reducer:(acc:number,curr:T)=>number =
+        typeof mapper === "function" ?
+        (acc,curr)=> acc + mapper(curr) :
+        (acc,curr)=> acc + +curr;
+
+    return nums.reduce(reducer,0);
+}
