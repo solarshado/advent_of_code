b8fdaa7ea59e93871f9fa6f68ffec67f72b31b7e
Adrian Todd <solarshado@users.noreply.github.com>
2024-11-23 15:38:26 -0500

    2015, day 1, parts 1 and 2 done

--- 2015/d1/Part1.cs
+++ 2015/d1/Part1.cs
@@ -3,7 +3,17 @@ namespace AoC2015.Day1;
 class Part1 {
 
     static void Main(string[] inputLines) {
-        Console.WriteLine("Day 1 Part 1");
+        foreach(var line in inputLines) {
+            int result = line.Aggregate(
+                    0,
+                    (acc,cur)=> acc + (cur switch {
+                        '(' => 1,
+                        ')' => -1,
+                        _ => throw new ArgumentException()
+                        })
+                    );
+            Console.WriteLine($"Result: {result}");
+        }
     }
 
 }
--- 2015/d1/Part2.cs
+++ 2015/d1/Part2.cs
@@ -3,7 +3,27 @@ namespace AoC2015.Day1;
 class Part2 {
 
     static void Main(string[] inputLines) {
-        Console.WriteLine("Day 1 Part 2");
-    }
+        foreach(var line in inputLines) {
+            var result = line.Aggregate(
+                    (acc: 0, pos: 0, result: -1),
+                    (acc,cur)=> {
 
+                    if(acc.result != -1)
+                        return acc;
+
+                    acc.pos++;
+                    acc.acc += (cur switch {
+                            '(' => 1,
+                            ')' => -1,
+                            _ => throw new ArgumentException()
+                            });
+
+                    if(acc.acc == -1 && acc.result == -1)
+                        acc.result = acc.pos;
+
+                    return acc;
+                    });
+            Console.WriteLine($"Result: {result.result}");
+        }
+    }
 }
