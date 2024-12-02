07cc7c17c3b3dcd5538d57948b361ed3c8b0b1c1
Adrian Todd <solarshado@users.noreply.github.com>
2024-12-02 00:26:26 -0500

    [2024] day 2, part 2, done!

---
+++ 2024/d2/part2.ts
@@ -0,0 +1,35 @@
+import { runMain, sum, } from "../util.ts";
+import { count, map, pairwise } from "../iter_util.ts";
+import { parseReport } from './part1.ts';
+
+function isReportSafe(report:number[], canRecurse=false):boolean {
+    const deltas = [...map(pairwise(report),([l,r])=>l-r)];
+
+    console.log(deltas);
+
+    const sign = Math.sign(deltas[0]);
+
+    return deltas.every(d=> Math.sign(d) === sign && (d=Math.abs(d), d > 0 && d < 4))
+        || (canRecurse && report.map(
+            (_,i)=>report.filter((__,j)=>j!==i)
+        ).some(r=>isReportSafe(r)) )
+}
+
+export async function main(lines:string[]) {
+    const cleanedLines = lines.map(l=>l.trim()).filter(l=>l!='');
+
+    const values = cleanedLines.map(parseReport);
+
+    console.log(values);
+
+    const safe = values.map(r=>isReportSafe(r,true));
+
+    console.log(safe);
+
+    const answer = sum(safe,b=>+b);
+
+    console.log(answer);
+}
+
+if(import.meta.main)
+    await runMain(main);
