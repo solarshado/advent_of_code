8e2a20c9b7cd733faefafad399d6237b3d4b8bec
Adrian Todd <solarshado@users.noreply.github.com>
2024-12-08 00:41:19 -0500

    [2024] day 8, part 1: done!

---
+++ 2024/d8/part1.ts
@@ -0,0 +1,76 @@
+import { runMain, sum, } from "../util.ts";
+import { count, map } from "../iter_util.ts";
+import * as gu from "../grid_util.ts";
+import { genPairs } from "../iter_util.ts";
+
+export type Transmitter = string & {length: 1};
+
+export type Grid = gu.Grid<Transmitter>;
+
+export function findTransmitters(grid:Grid):Map<Transmitter,Set<gu.Point>> {
+    const retVal = new Map<Transmitter,Set<gu.Point>>();
+
+    for(let y = 0; y < grid.length; ++y)
+    for(let x = 0; x < grid[0].length; ++x) {
+        const p = gu.getPointMultiton(x,y);
+        const tile = gu.getTileFrom(p,grid);
+
+        if(tile == '.')
+            continue;
+
+        if(!retVal.has(tile))
+            retVal.set(tile,new Set());
+
+        const set = retVal.get(tile)!;
+        set.add(p);
+    }
+
+    return retVal;
+}
+
+export function findAntiNodes(transmitters:Map<Transmitter,Set<gu.Point>>, grid:Grid) {
+
+    const retVal = new Set<gu.Point>();
+    
+    for(const [_,locs] of transmitters)
+    for(const [a,b] of genPairs([...locs])) {
+        const delta = gu.subtractPoints(b,a);
+
+        //d = b-a
+        //n1 = a - (b-a)
+        //n2 = b + (b-a)
+
+        const nodeOne = gu.subtractPoints(a,delta);
+        const nodeTwo = gu.addPoints(b,delta);
+
+        const nodes = [nodeOne,nodeTwo]
+            .filter(n=>gu.isPointOnGrid(n,grid))
+            .map(([x,y])=>gu.getPointMultiton(x,y));
+
+        for(const n of nodes)
+            retVal.add(n);
+    }
+
+    return retVal;
+}
+
+export async function main(lines:string[]) {
+    const cleanedLines = lines.map(l=>l.trim()).filter(l=>l!='');
+
+    const grid = gu.parseGrid<Transmitter>(cleanedLines);
+
+    const transmitters = findTransmitters(grid);
+
+    console.log(transmitters);
+
+    const nodes = findAntiNodes(transmitters, grid);
+    
+    console.log(nodes);
+
+    const answer = count(nodes);
+
+    console.log(answer);
+}
+
+if(import.meta.main)
+    await runMain(main);
