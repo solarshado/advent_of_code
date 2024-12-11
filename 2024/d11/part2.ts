f57bb074f27783f5ea5cb39598dc7dad4ac4c485
Adrian Todd <solarshado@users.noreply.github.com>
2024-12-11 01:01:30 -0500

    [2024] day 11: cleanup

--- 2024/d11/part2.ts
+++ 2024/d11/part2.ts
@@ -1,6 +1,6 @@
 import { runMain, sum, } from "../util.ts";
-import { count, map } from "../iter_util.ts";
-import { memoize, pipe, } from '../func_util.ts';
+import { map } from "../iter_util.ts";
+import { memoize, } from '../func_util.ts';
 import { Rule, RULES } from './part1.ts';
 
 function _applyOnce(n:number, rules:Rule[]=RULES):number[] {
@@ -12,10 +12,6 @@ function _applyOnce(n:number, rules:Rule[]=RULES):number[] {
         throw "no matching rule";
 }
 
-const applyOnce = memoize(_applyOnce);
-
-
-/// recursive, depth first?
 function applyRules(input:number[]):number {
     const MAX_DEPTH = 75;
 
@@ -23,7 +19,7 @@ function applyRules(input:number[]):number {
 
     function _inner(input:number, depth=0):number {
         const out = _applyOnce(input);
-        console.log("in inner",{input, out, depth});
+        //console.log("in inner",{input, out, depth});
         if(depth == MAX_DEPTH)
             return out.length;
 
@@ -32,9 +28,6 @@ function applyRules(input:number[]):number {
 
     return sum([...map(input,v=>inner(v,1))]);
 }
-
-// need to detect patterns in the process so we can skip ahead
-// there's no interdependency...
 
 export async function main(lines:string[]) {
     const cleanedLines = lines.map(l=>l.trim()).filter(l=>l!='');
@@ -45,22 +38,6 @@ export async function main(lines:string[]) {
 
     const answer = applyRules(values);
 
-    /// for each individual input, run the algo to completion (call apply 75 times)
-    // but will that work for even onw
-
-    //const answer = sum(values.map(v=>{
-    //    let vs = [v];
-    //    let blinks = 0;
-    //    while(true) {
-    //        console.log({blinks});
-    //        vs = applyRules(vs);
-    //        if(++blinks == 75)
-    //            break;
-    //    }
-    //
-    //    return vs.length;
-    //}));
-    //
     console.log(answer);
 }
 
