e83b1b0d949e48f68ec6393d09024d18131cbeda
Adrian Todd <solarshado@users.noreply.github.com>
2024-12-13 04:39:55 -0500

    [2024] day 13, part 2: done!

---
+++ 2024/d13/part2.ts
@@ -0,0 +1,109 @@
+import { gcd, lcm, runMain, splitArray, sum, } from "../util.ts";
+import { count, map } from "../iter_util.ts";
+import { memoize, pipe, } from '../func_util.ts';
+import * as gu from "../grid_util.ts";
+import { Machine, btnACost, btnBCost, mulPoint } from './part1.ts';
+
+function parseMachine(lines:string[]):Machine {
+    const [buttonA,buttonB,prize] = lines.map(l=> {
+        const [_,x,y] = Array.from(/.*?: X.(\d+), Y.(\d+)/.exec(l)!).map(Number);
+        return [x,y] as gu.Point;
+    });
+    return { buttonA, buttonB, prize: prize.map(c=>c + 10000000000000) as unknown as gu.Point };
+    //return { buttonA, buttonB, prize };
+}
+
+function findCheapestSolution(machine:Machine):number|false {
+    const {buttonA: [ax,ay], buttonB: [bx,by], prize: [px,py]} = machine;
+
+    /*
+    px = ax*A + bx*B 
+    py = ay*A + by*B 
+
+    solve for A
+
+    px -bx*B = ax*A 
+    (px - bx*B)/ax = A 
+    A = (px/ax) - (bx*B/ax)
+
+    for B
+    py = ay*((px - bx*B)/ax) + by*B 
+    py = (ay/ax)*(px - bx*B) + by*B 
+
+    py/B = ((ay/ax)*(px - bx*B))/B + by
+
+    ((ay/ax)*(px - bx*B))/B ----- aaaah
+
+    for B, try 2
+    py = ay*((px/ax) - (bx*B/ax)) + by*B 
+    py = (ay*px/ax) - (ay*bx*B/ax) + by*B 
+    py - (ay*px/ax) = (ay*bx*B/ax) + by*B 
+
+    ax*(py - (ay*px/ax)) = ax*((ay*bx*B/ax) + by*B)
+
+                = ay*bx*B + ax*by*B
+                = B*(ay*bx + ax*by)
+    ax*py - ay*px =
+
+    ax*py - ay*px = B*(ay*bx + ax*by)
+
+    (ax*py - ay*px)/(ay*bx + ax*by) = B
+    */
+    //const B = (ax*py - ay*px)/(ay*bx + ax*by);
+    //const A = (px/ax) - (bx*B/ax);
+
+    /// Cramer's rule, stolen from wikipedia's example
+    // ax*A + bx*B = px
+    // aka a1x + b1y = c1
+    //
+    // ay*A + by*B = py
+    // aka a2x + b2y = c2
+    //
+    // x = (c1b2 - b1c2)/(a1b2 - b1a2)
+    // aka
+    const A = (px*by - bx*py)/(ax*by - bx*ay)
+    // y = (a1c2 - c1a2)/(a1b2 - b1a2)
+    // aka
+    const B = (ax*py - px*ay)/(ax*by - bx*ay);
+
+    console.log({machine,A,B});
+
+    const ans = B + (3*A);
+
+    return ans !== Math.trunc(ans) ? false : ans;
+}
+
+export function pointModulo(n:gu.Point, mod:gu.Point):gu.Point {
+    return [
+        n[0] % mod[0],
+        n[1] % mod[1]
+    ];
+}
+
+export function pointBinOp(l:gu.Point, r:gu.Point, op:(l:number,r:number)=>number):gu.Point {
+    return [
+        op(l[0], r[0]),
+        op(l[1], r[1])
+    ];
+}
+
+export async function main(lines:string[]) {
+    const cleanedLines = lines.map(l=>l.trim())//.filter(l=>l!='');
+
+    const values = splitArray(cleanedLines,"").filter(a=> a.length === 3);
+
+    const machines = values.map(parseMachine);
+
+    //console.log(machines);
+
+    const solutions = machines.map(findCheapestSolution);
+
+    console.log(solutions);
+
+    const answer = sum(solutions, s=>s === false? 0 : s);
+
+    console.log(answer);
+}
+
+if(import.meta.main)
+    await runMain(main);
