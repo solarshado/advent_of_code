c078af1ca59538fd3e3fe1ffc11ce625b836852f
Adrian Todd <solarshado@users.noreply.github.com>
2024-12-22 00:23:51 -0500

    [2024] day 22, part 1: done!

---
+++ 2024/d22/part1.ts
@@ -0,0 +1,48 @@
+import { runMain, sum, } from "../util.ts";
+import { count, map, range } from "../iter_util.ts";
+import { memoize, pipe, } from '../func_util.ts';
+import * as gu from "../grid_util.ts";
+
+const ITERATIONS = 2000;
+
+export function nextSecret(secret:bigint):bigint {
+    secret = prune(mix(secret * 64n,secret));
+
+    // JS bitint division truncated towards zero... so shenanigans are needed here
+    //secret = prune(mix(BigInt(Math.round(Number(secret) / 32)), secret))
+    // "round towards zero" ... as long as we're >0, that's the same as truncating
+    secret = prune(mix(secret / 32n, secret));
+
+    secret = prune(mix(secret * 2048n,secret));
+
+    return secret;
+}
+
+function mix(mixin:bigint, secret:bigint) {
+    return mixin ^ secret;
+}
+
+const PRUNE_MODULO = 16777216n;
+function prune(secret:bigint, modulo=PRUNE_MODULO) {
+    return secret % modulo;
+}
+
+export async function main(lines:string[]) {
+    const cleanedLines = lines.map(l=>l.trim()).filter(l=>l!='');
+
+    let values = cleanedLines.map(BigInt);
+
+    console.log(values);
+
+    for(const _ of range(0,ITERATIONS))
+        values = values.map(nextSecret);
+
+    console.log(values);
+
+    const answer = values.reduce((l,r)=>l+r);
+
+    console.log(answer);
+}
+
+if(import.meta.main)
+    await runMain(main);
